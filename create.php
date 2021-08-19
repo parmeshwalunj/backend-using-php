3fb4ae25acb42e62af20c9211afa0e294d7326ed
parmesh <parmeshwalunj@gmail.com>
2021-08-19 12:02:18 +0530

    Added: Form Validation

--- create.php
+++ create.php
@@ -5,22 +5,35 @@ $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', ''
 // If Error while connecting DB throw Exception
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-echo $_SERVER['REQUEST_METHOD'].'<br>';
+$errors = [];
+$title = '';
+$price = '';
+$description = '';
+// echo $_SERVER['REQUEST_METHOD'].'<br>';
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $title = $_POST['title'];
     $description = $_POST['description'];
     $price = $_POST['price'];
     $date = date('Y-m-d H:i:s');
 
-    $statement = $pdo->prepare("INSERT INTO products (title, image, description, price, create_date)
-                                VALUES (:title, :image, :description, :price, :date)
-                                ");
-    $statement->bindValue(':title', $title);
-    $statement->bindValue(':image', "");
-    $statement->bindValue(':description', $description);
-    $statement->bindValue(':price', $price);
-    $statement->bindValue(':date', $date);
-    $statement->execute();
+
+    if(!$title){
+        $errors[] = 'Product title is required';
+    }
+    if(!$price){
+        $errors[] = 'Product price is required';
+    }
+    if(empty($errors)){
+        $statement = $pdo->prepare("INSERT INTO products (title, image, description, price, create_date)
+                                    VALUES (:title, :image, :description, :price, :date)
+                                    ");
+        $statement->bindValue(':title', $title);
+        $statement->bindValue(':image', "");
+        $statement->bindValue(':description', $description);
+        $statement->bindValue(':price', $price);
+        $statement->bindValue(':date', $date);
+        $statement->execute();
+    }
 }
 ?>
 <!doctype html>
@@ -42,6 +55,15 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
   </style>
   <body>
     <h1>Create new Product</h1>
+
+    <?php if(!empty($errors)): ?>
+    <div class="alert alert-danger">
+        <?php foreach ($errors as $error): ?>
+            <div><?php echo $error ?></div>
+        <?php endforeach; ?>
+    </div>
+    <?php endif; ?>
+
     <form action="" method="post">
     <div class="mb-3">
         <label>Product Image</label>
@@ -50,15 +72,15 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     </div>
     <div class="mb-3">
         <label>Product Title</label>
-        <input type="text" name="title" class="form-control">
+        <input type="text" name="title" class="form-control" value="<?php echo $title ?>">
     </div>
     <div class="mb-3">
         <label>Product Description</label>
-        <textarea class="form-control" name="description"></textarea>
+        <textarea class="form-control" name="description"><?php echo $description ?></textarea>
     </div>
     <div class="mb-3">
         <label>Product Price</label>
-        <input type="number" step=".01" name="price" class="form-control">
+        <input type="number" step=".01" name="price" class="form-control" value="<?php echo $price ?>">
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
     </form>
